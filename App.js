aab29329b534c026a578b0d47caaef1476160cc4
kidori <kidori@example.com>
2018-12-21 17:17:48 +0900

    Markdown setting2

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Image, ActivityInicator} from 'react-native';
+import Weather from './components/Weather';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>I love Ra~On</Text>
-      </View>
-    );
+class App extends Component{
+  state = {
+    isLoaded: false
+  };
+  
+  //로딩 뷰 15번 줄
+  //  {isLoaded ? null : (  ---- 참이면 null, 거짓이면 : 뒤 실행
+  render(){
+    const {isLoaded} = this.state;
+    return(
+        <View style={styles.container}>
+          {isLoaded ? null : ( 
+          <Weather/>
+          )}
+        </View>
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      container:{
+        flex:1,
+        backgroundColor:"skyblue", 
+      },
+      loading: {
+        flex:1,
+        backgroundColor:"#c8c8c8",
+        justifyContent:"flex-end",
+        paddingLeft: 25,
+      },
+      loadingText:{
+        fontSize: 38,
+        marginBottom: 100,
+      },
+      
+  
+    })
+
+export default App;
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This is an reactWeather app
 Author : Blockchain
 email-address : black9817@gmail.com
 
-![Alt text](/mdImage.jpg "Optional title")
+![Alt text](/mdImage.jpg "리엑트로 만든 날씨 앱")
