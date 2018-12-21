21c492c3123f69617eb3a120ff7bd3b0c698697a
kidori <kidori@example.com>
2018-12-21 14:54:49 +0900

    Markdown setting

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>I love Ra~On</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ README.md
@@ -0,0 +1,7 @@
+### reactWeather Native App
+This is an reactWeather app
+=============
+Author : Blockchain
+email-address : black9817@gmail.com
+
+![Alt text](/mdImage.jpg "Optional title")
